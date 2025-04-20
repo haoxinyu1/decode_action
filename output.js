e19a539e5ccdb157847dde148f1247ad326967b6
action <action@github.com>
2025-04-20 09:53:19 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,4 +1,4 @@
-//Sun Apr 20 2025 09:33:57 GMT+0000 (Coordinated Universal Time)
+//Sun Apr 20 2025 09:53:19 GMT+0000 (Coordinated Universal Time)
 //Base:<url id="cv1cref6o68qmpt26ol0" type="url" status="parsed" title="GitHub - echo094/decode-js: JS混淆代码的AST分析工具 AST analysis tool for obfuscated JS code" wc="2165">https://github.com/echo094/decode-js</url>
 //Modify:<url id="cv1cref6o68qmpt26olg" type="url" status="parsed" title="GitHub - smallfawn/decode_action: 世界上本来不存在加密，加密的人多了，也便成就了解密" wc="741">https://github.com/smallfawn/decode_action</url>
 eval(function (_0x3ff715, _0x2b8f04, _0x1c9eaa, _0x241d3c, _0x28649b, _0x4edc8d) {
